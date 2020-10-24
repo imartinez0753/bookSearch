27c49c380d81a7ffda2add27eef482cf9c831a67
Ian Martinez <imartinez0753@gmail.com>
2020-10-23 17:26:07 -0700

    house cleaning

--- booksearch/client/src/App.js
+++ booksearch/client/src/App.js
@@ -3,18 +3,17 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Search from "./pages/search/index"
 import Fav from "./pages/fav/index"
 import NavBar from "./components/navBar/navBar"
-import { StoreProvider } from "./utils/GlobalState";
 
 function App() {
   return (
     <Router>
       <div>
-        <StoreProvider>
+        
         <NavBar />      
       <Route exact path="/" component={Search} />
         <Route exact path="/fav" component={Fav} />
 
-        </StoreProvider>
+       
       
       </div>
       </Router>
