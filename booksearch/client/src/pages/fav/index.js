375fa3aafb4d98119f78de925fe0c4dfd2601a30
Ian Martinez <imartinez0753@gmail.com>
2020-10-23 17:24:03 -0700

    it livesgit add -Agit add -A

--- booksearch/client/src/pages/fav/index.js
+++ booksearch/client/src/pages/fav/index.js
@@ -2,49 +2,77 @@ import React, { Component } from "react";
 import { Row, Col, Container, Card, Button } from "react-bootstrap";
 import API from "../../utils/API"
 
+
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      bookRes: []
+    };
+  }
+ 
 
   getFav = () => {
     API. getFavBooks().then((res) => {
-      console.log(res);
+      console.log(res.data);
+      this.setState({...this.state, bookRes: res.data})
     })
     .catch((err) => console.log(err))
   }
-    
-render () {
-    return (
-      <div>
-          <h1>Fav for book</h1>
-        
-          <Card>
-{this.state.bookRes.map((book) => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Card>
-          <Card.Img style={{ width: '8rem', height: '8rem' }}     
-     src={book.pic} /> 
-    <Card.Title>{book.name}</Card.Title>      
-   <Card.Body>{book.descript}</Card.Body>
-   <Button 
-   id={book.id}
-   onClick={(event) => this.postFav(event)}
-   variant="primary">Fav</Button>
-          </Card>        
-        </li>      
-      </ul>
-      <br />
-    </div>    
-  )
-})}
-</Card>
+  handleButtonClick = (event) => {
+    event.preventDefault();
+    this.getFav()
 
-</div>
-     
-    )
   }
-}
+  render () {
+
+    return(
+      <div>
+        <Button
+        onClick = {this.handleButtonClick}
+        >find favs</Button>
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              {this.state.bookRes.map((book) => {
+                return (
+                  <div>
+                    <ul>
+                      <li>
+                        <Card>
+                          <Card.Img style={{ width: '8rem', height: '8rem' }}     
+                            src={book.pic} /> 
+                              <Card.Title>{book.name}</Card.Title>      
+                                <Card.Body>{book.descript}</Card.Body>
+                                  <Button 
+                                    id={book.id}
+                                    onClick={(event) => this.postFav(event)}
+                                    variant="primary">Fav</Button>
+                          </Card>        
+                       </li>      
+                    </ul>
+                    <br />
+                  </div>    
+                        )
+                    })}
+                </Card>
+              </Col>
+            </Row>
+        </Container> 
+
+  </div>
+    )
+   
+
+
+
+  }
+    
+    
+}   
+
     
 
 export default Search;
