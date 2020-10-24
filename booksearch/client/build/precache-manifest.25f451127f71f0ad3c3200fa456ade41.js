9687bbbfc58e45bc3fe20d3a730e49ca81b4116c
Ian Martinez <imartinez0753@gmail.com>
2020-10-24 15:28:51 -0700

    failing to add files to git

---
+++ booksearch/client/build/precache-manifest.25f451127f71f0ad3c3200fa456ade41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc9fecbbe7f6e18d0d055a9781b1623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6411441a047dd5850bd9",
+    "url": "/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "6411441a047dd5850bd9",
+    "url": "/static/js/2.f15f9ea6.chunk.js"
+  },
+  {
+    "revision": "80097c1312432faada68f848f38927c4",
+    "url": "/static/js/2.f15f9ea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f483bbc56803e4204b",
+    "url": "/static/js/main.b691fb35.chunk.js"
+  },
+  {
+    "revision": "2b8f580612459d80dc88",
+    "url": "/static/js/runtime-main.f65105ae.js"
+  }
+]);
